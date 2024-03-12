c46c59779b9192bf0b5994b18f57c335b16b9836
blueempire-design <163209268+blueempire-design@users.noreply.github.com>
2024-03-12 19:43:39 -0300

    Home page

---
+++ app.js
@@ -0,0 +1,66 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+const lightbox = document.querySelector(".image-gallery-container .lightbox");
+const lightboxImage = document.querySelector(
+  ".image-gallery-container .lightbox img"
+);
+const lightboxTitle = document.querySelector(
+  ".image-gallery-container .lightbox .title"
+);
+const downloadBtn = document.querySelector(
+  ".image-gallery-container .download-btn"
+);
+const nextBtn = document.querySelector(".image-gallery-container .next-btn");
+const previousBtn = document.querySelector(
+  ".image-gallery-container .previous-btn"
+);
+const closeBtn = document.querySelector(".image-gallery-container .close-btn");
+
+let currentImage = "";
+
+const showImage = (data) => {
+  currentImage = data;
+  lightbox.classList.add("active");
+  let image = data.querySelector("img");
+  let title = data.querySelector(".title").innerText;
+
+  lightboxImage.src = image.src;
+  downloadBtn.href = image.src;
+
+  if (title) {
+    lightboxTitle.innerText = title;
+  }
+};
+
+closeBtn.addEventListener("click", () => {
+  lightbox.classList.remove("active");
+});
+
+nextBtn.addEventListener("click", () => {
+  if (currentImage.nextElementSibling) {
+    currentImage = currentImage.nextElementSibling;
+    showImage(currentImage);
+  }
+});
+
+previousBtn.addEventListener("click", () => {
+  if (currentImage.previousElementSibling) {
+    currentImage = currentImage.previousElementSibling;
+    showImage(currentImage);
+  }
+});
